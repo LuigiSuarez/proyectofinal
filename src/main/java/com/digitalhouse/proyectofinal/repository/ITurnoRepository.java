bfbb1d071bec21613dd6b08db8e820e6ab6b51ef
LinaMVG <limarcegarzon@gmail.com>
2024-09-09 22:09:59 -0500

    correction ITurnoRepository atribut from Odontologo

--- src/main/java/com/digitalhouse/proyectofinal/repository/ITurnoRepository.java
+++ src/main/java/com/digitalhouse/proyectofinal/repository/ITurnoRepository.java
@@ -17,6 +17,6 @@ public interface ITurnoRepository extends JpaRepository<Turno,Integer> {
     @Query("Select t from Turno t where t.fecha between :fechaInicio AND :fechaFin")
     Optional<Turno> buscarTurnosPorRangoDeFechas(LocalDate fechaInicio, LocalDate fechaFin);
 
-    @Query("Select t from Turno t join t.odontologo o where o.apellido = :odontologoApellido ")
+    @Query("Select t from Turno t join t.odontologo o where o.Apellido = :odontologoApellido ")
     Optional<Turno> buscarTurnosPorApellidoOdontologo(String odontologoApellido);
 }
